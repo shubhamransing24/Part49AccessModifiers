b8b24bcbfd12ee0f9f245bf3dd5a416721177988
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-08 12:30:21 +0530

    Add project files.

---
+++ ACL/Acl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ACL
+{
+    public class Acl
+    {
+
+        public int id = 10;
+        static protected string s="Shubham";
+        protected internal void Ipl()
+        {
+            Console.WriteLine("Ipl 2021 Winner is Rcb");
+        }
+        internal void MostWickets()
+        {
+            Console.WriteLine("Harshal Patel");
+        }
+        
+    }
+    public class B : Acl
+    { 
+       
+        public void Print()
+        {
+            B b1 = new B();
+            Console.WriteLine(Acl.s);
+            b1.MostWickets();
+        }
+    }
+}
---
+++ Part49AccessModifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ACL;
+namespace Part49AccessModifiers
+{
+    class Program :Acl
+    {
+
+        static void Main(string[] args)
+        {
+            Acl xyz = new Acl();
+            Console.WriteLine(xyz.id);
+            B b = new B();
+            b.Print();
+            Program p = new Program();
+            p.Ipl();
+            
+            
+        }
+    }
+}
